3c5cb2b85d5a35db4fb252870fd350d2322116f5
gabriel <g.inaciobarbosa@gmail.com>
2015-10-06 23:56:38 -0300

    Readme corrigido

--- src/api/flows_handler_classes.py
+++ src/api/flows_handler_classes.py
@@ -1,16 +1,22 @@
 
 import json
 import web
-#import re
+import re
+import app.ryu_flows_pusher_classes as ryu_con
+import app.pyke_facts_insert_classes as pyke
 
 class FlowsBlockIP:
     def POST():
         msg = web.data()
         ip = json.loads(msg)
+        ryu_con.insert_block_by_ip(ip)
 
     def DELETE():
         msg = web.data()
         ip = json.loads(msg)
+        ryu_con.remove_block_by_ip(ip)
+
+
 """
 class FlowsBlockMAC:
     def POST():
--- src/app/ryu_flows_pusher_classes.py
+++ src/app/ryu_flows_pusher_classes.py
@@ -1,32 +1,9 @@
 
-import ryu.app.ofctl_rest as ryu
+import ryu.app.ofctl_rest as ryu_con
 
 def insert_block_by_ip(ip):
-    flow_format = """{
-    "dpid": 1,
-    "cookie": 1,
-    "cookie_mask": 1,
-    "table_id": 0,
-    "idle_timeout": 30,
-    "hard_timeout": 30,
-    "priority": 11111,
-    "flags": 1,
-    "match":{
-        "in_port":1
-    }
- }""".replace('target_ip', ip)
+    uri = '/stats/flowentry/add'
+    flow_format = """ """.replace('target_ip', ip)
 
- def remove_block_by_ip(ip):
-     flow_format = """{
-    "dpid": 1,
-    "cookie": 1,
-    "cookie_mask": 1,
-    "table_id": 0,
-    "idle_timeout": 30,
-    "hard_timeout": 30,
-    "priority": 11111,
-    "flags": 1,
-    "match":{
-        "in_port":1
-    }
- }""".replace('target_ip', ip)
+def remove_block_by_ip(ip):
+    flow_format = """ """.replace('target_ip', ip)
