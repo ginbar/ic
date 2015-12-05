b82239f1b3ddd1e0c059fa467dd482af6a680a98
gabriel <g.inaciobarbosa@gmail.com>
2015-12-05 20:28:07 -0200

    comunicao com o ryu corrigida

--- src/app/ryu_flows_pusher_classes.py
+++ src/app/ryu_flows_pusher_classes.py
@@ -8,66 +8,66 @@ def uri_format():
     return 'http://localhost:8080/uri'
 
 def insert_block_by_ip(ip):
-    print 'insert_block_by_ip foi invocado com ip: ' + ip
     uri = 'stats/flowentry/add'
     flow_format = {
         "dpid": dp_id,
         "match":{"ipv4_dst":ip},
         "actions":[{"type":"OUTPUT", "port": 2}]
     }
-    requests.post(uri_format().replace('uri', uri), flow_format)
+    msg = str(flow_format)
+    requests.post(uri_format().replace('uri', uri), msg)
 
 
 def remove_block_by_ip(ip):
-    print 'remove_block_by_ip foi invocado com mac: ' + ip
     uri = 'stats/flowentry/delete_strict'
     flow_format = {
         "dpid": dp_id,
         "match":{"ipv4_dst":ip},
         "actions":[{"type":"OUTPUT", "port": 2}]
     }
-    requests.post(uri_format().replace('uri', uri), flow_format)
+    msg = str(flow_format)
+    requests.post(uri_format().replace('uri', uri), msg)
 
 
 def insert_block_by_mac(mac):
-    print 'insert_block_by_mac foi invocado com mac: ' + mac
     uri = 'stats/flowentry/add'
     flow_format = {
         "dpid": dp_id,
         "match":{"eth_dst":mac},
         "actions":[{"type":"OUTPUT", "port": 2}]
     }
-    requests.post(uri_format().replace('uri', uri), flow_format)
+    msg = str(flow_format)
+    requests.post(uri_format().replace('uri', uri), msg)
 
 
 def remove_block_by_mac(mac):
-    print 'insert_block_by_mac foi invocado com mac: ' + mac
     uri = 'stats/flowentry/delete_strict'
     flow_format = {
         "dpid": dp_id,
         "match":{"eth_dst":mac},
         "actions":[{"type":"OUTPUT", "port": 2}]
     }
-    requests.post(uri_format().replace('uri', uri), flow_format)
+    msg = str(flow_format)
+    requests.post(uri_format().replace('uri', uri), msg)
 
 
 def insert_block_by_port(port):
-    print 'insert_block_by_mac foi invocado com mac: ' + mac
     uri = 'stats/flowentry/add'
     flow_format = {
         "dpid": dp_id,
         "match":{"tp_dst":port},
         "actions":[{"type":"OUTPUT", "port": 2}]
     }
-    requests.post(uri_format().replace('uri', uri), flow_format)
+    msg = str(flow_format)
+    requests.post(uri_format().replace('uri', uri), msg)
 
 
 def remove_block_by_port(port):
-    print 'insert_block_by_mac foi invocado com mac: ' + mac
     uri = 'stats/flowentry/delete_strict'
     flow_format = {
         "dpid": dp_id,
         "match":{"tp_dst":port},
         "actions":[{"type":"OUTPUT", "port": 2}]
     }
-    requests.post(uri_format().replace('uri', uri), flow_format)
+    msg = str(flow_format)
+    requests.post(uri_format().replace('uri', uri), msg)
